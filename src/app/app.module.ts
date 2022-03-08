9c0b35ef282fac1ae59babb971a477a1c9d3910d
realbenben <b.patou59@gmail.com>
2022-03-09 00:22:55 +0100

    add simple component as exemple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './form/form.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
