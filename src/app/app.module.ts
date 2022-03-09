8e654bf294ac720f74b0442e7f2de9e2e00cc32c
Kevin Arnaud <kevin.arnaud.addpro@gmail.com>
2022-03-09 16:38:03 +0100

    merge ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { ConctactComponent } from './conctact/conctact.component';
-=======
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormComponent } from './form/form.component';
 import { FooterComponent } from './footer/footer.component';
->>>>>>> fc72b73d3c0e12b26d3a4753f656a095fe212f03
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    ConctactComponent
-=======
+    ConctactComponent,
     NavbarComponent,
     FormComponent,
     FooterComponent
->>>>>>> fc72b73d3c0e12b26d3a4753f656a095fe212f03
   ],
   imports: [
     BrowserModule,
