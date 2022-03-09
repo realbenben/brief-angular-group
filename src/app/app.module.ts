78b1b4a367089857e75ab8f5c26190fe86da678f
abiba12 <ndeyediop.tech@gmail.com>
2022-03-09 16:09:52 +0100

    fisrt commit on aby

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConctactComponent } from './conctact/conctact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConctactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/conctact/conctact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conctact',
+  templateUrl: './conctact.component.html',
+  styleUrls: ['./conctact.component.scss']
+})
+export class ConctactComponent implements OnInit {
+<app-contact > </app-contact>
+constructor() { }
+
+ngOnInit(): void {
+}
+
+}
