e58eb6a1c8a36fa7e8a5264381307d5f9966bacf
realbenben <b.patou59@gmail.com>
2022-03-10 10:29:12 +0100

    gallery html done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FormComponent } from './form/form.component';
 import { FooterComponent } from './footer/footer.component';
 import { MissionsComponent } from './missions/missions.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MissionsComponent } from './missions/missions.component';
     NavbarComponent,
     FormComponent,
     FooterComponent,
-    MissionsComponent
+    MissionsComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
